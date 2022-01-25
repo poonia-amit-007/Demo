bd07c8174b21d6ea93917769b0a6664fa6c713a7
poonia92 <amit.kumar38@oyorooms.com>
2022-01-25 20:14:52 +0530

    Added jacoco

---
+++ src/main/java/com/example/demo/TempClass.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+public class TempClass {
+
+    public int multiplyByTwo(int value) {
+        return 2*value;
+    }
+
+    public int multiplyByThree(int value) {
+        return 3*value;
+    }
+}
---
+++ src/test/java/com/example/demo/TempClassTest.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class TempClassTest {
+
+
+    private TempClass tempClass = new TempClass();
+
+    @Test
+    public void testMultiplyByTwo() {
+        int expected = 4;
+        int actual = tempClass.multiplyByTwo(2);
+        assertEquals(expected, actual);
+    }
+
+}
